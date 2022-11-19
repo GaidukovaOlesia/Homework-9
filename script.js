5406b3805db2362e25627839025b4064839504d9
GaidukovaOlesia <lesyagaydukova@gmail.com>
2022-11-19 23:30:57 +0200

    homework 9

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-
+debugger;
 arr = [];
 arrLength = parseInt(prompt("Please, enter length of new array!"));
-arrItem = prompt("Please, enter elements of array!").replaceAll(" ", "");
-for(i = 0; i < arrLength; i++) {
-    arr[i] = prompt("Please, enter elements of array!").replaceAll(" ", "");
-    if(arr[i] === null) {
+
+for(let i = 0; i < arrLength; i++) {
+    arr.push(prompt("Please, enter elements of array!").replaceAll(" ", ""));
+    if(!arr[i]) {
         alert("Enter elements!")
-    } else if( arr[i] === " ") {
-        alert("You must enter element!")
     }
 }
 alert(`You create array ${arr}`);
 console.log(arr);
+for(i = 0; i < arr.length; i++) {
+    for(j = 0; j < arr.length - 1; j++){
+        if(arr[j] > arr[j+1]) {
+           let value = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = value
+        }
+    }
+}
+console.log(arr);
 
-sortArr = arr.sort();
-alert((`We have sorted the array in ascending order ${sortArr}`));
-console.log(sortArr);
 
-newArr = sortArr.splice(2, 3);
+newArr = arr.splice(2, 3);
 alert(`We removed elements from the array from 2 to 4: ${newArr}`);
-console.log(sortArr);
+console.log(arr);
